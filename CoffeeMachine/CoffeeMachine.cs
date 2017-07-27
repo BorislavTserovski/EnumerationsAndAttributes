8300f20dc91bf0cbac79ee032f02cc16e1c1f6cd
Borislav Tserovski <Borislav.Tserovski@gmail.com>
2017-07-27 15:18:58 +0300

    Coffee

---
+++ CoffeeMachine/CoffeeMachine.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class CoffeeMachine
+{
+    public List<CoffeeType> CoffeesSold { get; }
+    public int Coins { get; set; }
+    public CoffeeMachine()
+    {
+        this.CoffeesSold = new List<CoffeeType>();
+    }
+    public void BuyCoffee(string price, string type)
+    {
+        CoffeeType coffeeType = (CoffeeType) Enum.Parse(typeof(CoffeeType), type);
+        CoffeePrice coffeePrice = (CoffeePrice) Enum.Parse(typeof(CoffeePrice), price);
+
+        if (this.Coins>=(int)coffeePrice)
+        {
+            this.CoffeesSold.Add(coffeeType);
+            this.Coins = 0;
+        }
+    }
+    public void InsertCoin(string coin)
+    {
+        Coin toRemove = (Coin) Enum.Parse(typeof(Coin), coin);
+        this.Coins += (int) toRemove;
+    }
+}
+
---
+++ CoffeeMachine/CoffeePrice.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public enum CoffeePrice
+{
+    None = 0,
+    Small = 50,
+    Normal = 100,
+    Double = 200
+}
+
