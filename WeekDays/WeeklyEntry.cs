880306274205ea1c726c8d3eb827058128d8f3af
Borislav Tserovski <Borislav.Tserovski@gmail.com>
2017-07-27 14:55:31 +0300

    WeekDays

---
+++ WeekDays/WeeklyEntry.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class WeeklyEntry: IComparable<WeeklyEntry>
+{
+
+    public WeeklyEntry(string weekDay, string notes)
+    {
+        this.WeekDay = (WeekDay) Enum.Parse(typeof(WeekDay), weekDay);
+        this.Notes = notes;
+    }
+
+    public WeekDay WeekDay { get; set; }
+    public string Notes { get; set; }
+
+    public override string ToString()
+    {
+        return $"{this.WeekDay} - {this.Notes}";
+    }
+
+    public int CompareTo(WeeklyEntry other)
+    {
+        if (ReferenceEquals(this, other))
+        {
+            return 0;
+        }
+        if (ReferenceEquals(null, other))
+        {
+            return 1;
+        }
+        var weekDayCompare = WeekDay.CompareTo(other.WeekDay);
+        if (weekDayCompare!=0)
+        {
+            return weekDayCompare;
+        }
+        return string.Compare(Notes, other.Notes, StringComparison.Ordinal);
+    }
+}
+
